75ec259af7be26725a3c296149f77e0eadc52e7f
Wolf Developer <lupo.andrea.93@gmail.com>
2022-12-04 12:13:01 +0100

    Add csv file reader class

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,38 @@
-import fs from 'fs';
+import { CsvFileReader } from "./CsvFileReader";
 
-const matches = fs.readFileSync('football.csv', {
-  encoding: 'utf-8'
-})
-  .split('\n')
-  .map((row: string): string[] => {
-    return row.split(',');
-  });
+const reader = new CsvFileReader('football.csv');
+reader.read();
+/* 
+const homeWin = 'H';
+const awayWin = 'A';
+const draw = 'D';
+ */
+/* 
+const MatchResult = {
+  HomeWIn: 'H',
+  AwayWin: 'A',
+  Draw: 'D'
+}; 
+*/
 
-console.log(matches);
+enum MatchResult {
+  HomeWIn = 'H',
+  AwayWin = 'A',
+  Draw = 'D'
+};
+
+let manUnitedWins = 0;
+
+for (let match of reader.data) {
+  // result is in position 5
+  const team1: string = match[1];
+  const team2: string = match[2];
+  const result: string = match[5];
+  if (team1 === 'Man United' && result === MatchResult.HomeWIn) {
+    manUnitedWins++;
+  } else if (team2 === 'Man United' && result === MatchResult.AwayWin) {
+    manUnitedWins++;
+  }
+}
+
+console.log(`Man United won ${manUnitedWins} games`);
